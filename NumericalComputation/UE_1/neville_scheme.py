88afbb0fa57a26eacb1d13113ecf055007ccaf11
BuildTools <unconfigured@null.spigotmc.org>
2019-10-09 20:28:10 +0200

    <Numerical Computation>
    initial commit of 1.UE

---
+++ NumericalComputation/UE_1/neville_scheme.py
@@ -0,0 +1,47 @@
+import numpy as np
+from matplotlib import pyplot
+
+def neville_scheme(knotes, f, x):
+
+    n = len(knotes)
+
+    p = np.zeros((n,n))
+
+    for k in range(n):
+        for i in range(n-k):
+            if k == 0:
+                p[k, i] = f[i]
+            else:
+                p[k, i] = ((x - knotes[i+k])*p[k-1, i] - (x - knotes[i])*p[k-1, i+1])/(knotes[i]-knotes[i+k])
+
+    return p
+
+
+knotes = np.array([0, 1, 4])
+values = np.array([0, 2, 8])
+x = 3
+
+p = neville_scheme(knotes, values, x)
+print(np.transpose(p))
+
+
+def f(h):
+    return (np.exp(h) - 1)/h
+
+I = 10
+knotes = np.empty(I)
+values = np.empty(I)
+
+for i in range(I):
+    knotes[i] = np.exp2(-i)
+    values[i] = f(knotes[i])
+
+p = neville_scheme(knotes, values, 0)
+p = np.transpose(p)
+print(np.transpose(p))
+
+pyplot.loglog(knotes[0:I], abs(p[0:I,0]-1))
+pyplot.loglog(knotes[0:I-1], abs(p[0:I-1,1]-1))
+pyplot.loglog(knotes[0:I-2], abs(p[0:I-2,2]-1))
+
+pyplot.show()
